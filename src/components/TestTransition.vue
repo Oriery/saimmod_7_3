236ca6b45cc4a51f1bf53157346b64d071b2da5e
Oriery <panev291002@gmail.com>
2023-10-17 14:22:56 +0300

    use only one child

--- src/components/TestTransition.vue
+++ src/components/TestTransition.vue
@@ -5,32 +5,21 @@
     class="min-h-[10rem]"
   >
     <div
-      v-if="child0ShouldExist"
-      v-show="child0ShouldBeVisible"
-      ref="child0"
-      @click="move(true)"
+      ref="child"
     >
-      Move me to Parent 2
+      Move me to another Parent
     </div>
   </div>
   <div
     id="parent1"
     ref="parent1"
     class="min-h-[10rem]"
-  >
-    <div
-      v-if="child1ShouldExist"
-      v-show="child1ShouldBeVisible"
-      ref="child1"
-      @click="move(false)"
-    >
-      Move me to Parent 1
-    </div>
-  </div>
+  ></div>
+  <v-btn @click="move(inParent0)">Move to another place</v-btn>
 </template>
 
 <script setup lang="ts">
-import { ref, nextTick, watch } from 'vue'
+import { ref, nextTick } from 'vue'
 import type { Ref } from 'vue'
 
 const TRANSITION_DURATION = 0.3
@@ -39,13 +28,8 @@ const transitionInProgress = ref(false)
 
 const parent0 = ref<HTMLElement | null>(null)
 const parent1 = ref<HTMLElement | null>(null)
-const child0 = ref<HTMLElement | null>(null)
-const child1 = ref<HTMLElement | null>(null)
+const child = ref<HTMLElement | null>(null)
 const inParent0 = ref(true)
-const child0ShouldExist = ref(true)
-const child1ShouldExist = ref(false)
-const child0ShouldBeVisible = ref(true)
-const child1ShouldBeVisible = ref(false)
 
 const getRect = (element: HTMLElement) => element.getBoundingClientRect()
 
@@ -53,33 +37,27 @@ const move = async (toParent1: boolean) => {
   if (transitionInProgress.value) return
   transitionInProgress.value = true
 
-  if (toParent1) {
-    child0ShouldExist.value = true
-    child1ShouldExist.value = true
-    child0ShouldBeVisible.value = true
-    child1ShouldBeVisible.value = false
-  } else {
-    child0ShouldExist.value = true
-    child1ShouldExist.value = true
-    child0ShouldBeVisible.value = false
-    child1ShouldBeVisible.value = true
-  }
-
-  await nextTick()
-
-  if (child0.value && child1.value && parent0.value && parent1.value) {
+  if (child.value && parent0.value && parent1.value) {
     const htmlElementFrom = (toParent1 ? parent0 : parent1) as Ref<HTMLElement>
     const htmlElementTo = (toParent1 ? parent1 : parent0) as Ref<HTMLElement>
-    const initChild = (toParent1 ? child0 : child1) as Ref<HTMLElement>
-    const finalChild = (toParent1 ? child1 : child0) as Ref<HTMLElement>
+    const initChild = child as Ref<HTMLElement>
 
     const initialRect = getRect(htmlElementFrom.value)
-    const finalRect = getRect(htmlElementTo.value)
+
+    const tempChild = ref(document.createElement('div'))
+    htmlElementTo.value.appendChild(tempChild.value)
+    const finalRect = getRect(tempChild.value)
+    tempChild.value.remove()
+    console.log(`initialRect: ${JSON.stringify(initialRect)}`)
+    console.log(`finalRect: ${JSON.stringify(finalRect)}`)
 
     const dx = finalRect.left - initialRect.left
     const dy = finalRect.top - initialRect.top
 
     console.log(`dx: ${dx}, dy: ${dy}`)
+
+    const tempStyleTransform = initChild.value.style.transform
+    const tempStyleTransition = initChild.value.style.transition
 
     initChild.value.style.transition = `transform ${TRANSITION_DURATION}s ease-in-out`
     initChild.value.style.transform += ' ' + `translate(${dx}px, ${dy}px)`
@@ -89,32 +67,20 @@ const move = async (toParent1: boolean) => {
       'transitionend',
       async (e) => {
         inParent0.value = !inParent0.value
+        htmlElementTo.value.appendChild(initChild.value)
+
+        initChild.value.style.transform = tempStyleTransform
+        initChild.value.style.transition = tempStyleTransition
 
         transitionInProgress.value = false
-
-        console.log('inParent0 3', inParent0.value)
       },
       { once: true },
     )
   } else {
     console.error('One of the elements is null')
-    inParent0.value = !!inParent0.value
 
     transitionInProgress.value = false
   }
 }
 
-watch(inParent0, (newVal) => {
-  if (newVal) {
-    child0ShouldExist.value = true
-    child1ShouldExist.value = false
-    child0ShouldBeVisible.value = true
-    child1ShouldBeVisible.value = false
-  } else {
-    child0ShouldExist.value = false
-    child1ShouldExist.value = true
-    child0ShouldBeVisible.value = false
-    child1ShouldBeVisible.value = true
-  }
-})
 </script>
