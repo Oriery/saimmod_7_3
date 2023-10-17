b52dae208c6280a94381d7827d091b5465cf12ba
Oriery <panev291002@gmail.com>
2023-10-17 15:16:20 +0300

    move function for animation into separate file

--- src/components/TestTransition.vue
+++ src/components/TestTransition.vue
@@ -32,72 +32,25 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Ref } from 'vue'
-
-const transitionInProgress = ref(false)
+import changeParentSmoothly from '../utils/changeParentSmoothly'
 
 const parent0 = ref<HTMLElement | null>(null)
 const parent1 = ref<HTMLElement | null>(null)
 const child = ref<HTMLElement | null>(null)
 const inParent0 = ref(true)
 
-const getRect = (element: HTMLElement) => element.getBoundingClientRect()
-
-const move = async (child: Ref<HTMLElement>, toParent: Ref<HTMLElement>, transitionTime: number = 400) => {
-  if (transitionInProgress.value) return
-  transitionInProgress.value = true
-
-  if (child.value) {
-    const initialRect = getRect(child.value)
-
-    const tempChild = ref(document.createElement('div'))
-    toParent.value.appendChild(tempChild.value)
-
-    const finalRect = getRect(tempChild.value)
-
-    tempChild.value.remove()
-
-    const dx = finalRect.left - initialRect.left
-    const dy = finalRect.top - initialRect.top
-
-    console.log(`dx: ${dx}, dy: ${dy}`)
-
-    const tempStyleTransform = child.value.style.transform
-    const tempStyleTransition = child.value.style.transition
-
-    child.value.style.transition = `transform ${transitionTime}ms ease-in-out`
-    child.value.style.transform += ' ' + `translate(${dx}px, ${dy}px)`
-
-    // add handler to wait for transition to end
-    child.value.addEventListener(
-      'transitionend',
-      async (e) => {
-        inParent0.value = !inParent0.value
-        toParent.value.appendChild(child.value)
-
-        child.value.style.transform = tempStyleTransform
-        child.value.style.transition = tempStyleTransition
-
-        transitionInProgress.value = false
-      },
-      { once: true },
-    )
-  } else {
-    console.error('One of the elements is null')
-
-    transitionInProgress.value = false
-  }
-}
-
-function toggle() {
+async function toggle() {
   if (!parent1.value || !parent0.value || !child.value) {
     console.error('One of the objects is null')
     return
   }
 
   if (inParent0.value) {
-    move(child as Ref<HTMLElement>, parent1 as Ref<HTMLElement>)
+    await changeParentSmoothly(child as Ref<HTMLElement>, parent1 as Ref<HTMLElement>)
+    inParent0.value = false
   } else {
-    move(child as Ref<HTMLElement>, parent0 as Ref<HTMLElement>)
+    await changeParentSmoothly(child as Ref<HTMLElement>, parent0 as Ref<HTMLElement>)
+    inParent0.value = true
   }
 }
 </script>
---
+++ src/utils/changeParentSmoothly.ts
@@ -0,0 +1,61 @@
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+const getRect = (element: HTMLElement) => element.getBoundingClientRect()
+
+const currentlyProcessedHtmlElements: HTMLElement[] = []
+
+export default async function changeParentSmoothly(
+  child: Ref<HTMLElement>,
+  toParent: Ref<HTMLElement>,
+  transitionTime: number = 400,
+) {
+  if (currentlyProcessedHtmlElements.includes(child.value)) {
+    throw new Error('This element is already being moved')
+  }
+  currentlyProcessedHtmlElements.push(child.value)
+
+  try {
+    if (child.value) {
+      const initialRect = getRect(child.value)
+
+      const tempChild = ref(document.createElement('div'))
+      toParent.value.appendChild(tempChild.value)
+
+      const finalRect = getRect(tempChild.value)
+
+      tempChild.value.remove()
+
+      const dx = finalRect.left - initialRect.left
+      const dy = finalRect.top - initialRect.top
+
+      console.log(`dx: ${dx}, dy: ${dy}`)
+
+      const tempStyleTransform = child.value.style.transform
+      const tempStyleTransition = child.value.style.transition
+
+      child.value.style.transition = `transform ${transitionTime}ms ease-in-out`
+      child.value.style.transform += ' ' + `translate(${dx}px, ${dy}px)`
+
+      await new Promise((resolve) => {
+        // add handler to wait for transition to end
+        child.value.addEventListener(
+          'transitionend',
+          async (e) => {
+            toParent.value.appendChild(child.value)
+
+            child.value.style.transform = tempStyleTransform
+            child.value.style.transition = tempStyleTransition
+
+            resolve(e)
+          },
+          { once: true },
+        )
+      })
+    } else {
+      console.error('One of the elements is null')
+    }
+  } finally {
+    currentlyProcessedHtmlElements.splice(currentlyProcessedHtmlElements.indexOf(child.value), 1)
+  }
+}
