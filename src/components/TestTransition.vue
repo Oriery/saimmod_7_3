0a339fc6679a0675252fb944ae52811c676791a1
Oriery <panev291002@gmail.com>
2023-10-17 14:57:52 +0300

    fix bug; generalize

--- src/components/TestTransition.vue
+++ src/components/TestTransition.vue
@@ -1,28 +1,37 @@
 <template>
-  <div
-    id="parent0"
-    ref="parent0"
-    class="min-h-[10rem]"
-  >
-    <div
-      ref="child"
-    >
-      Move me to another Parent
+  <div class="flex flex-col gap-2">
+    <div class="grid grid-cols-2 w-full gap-2">
+      <div
+        id="parent0"
+        ref="parent0"
+        class="flex flex-col gap-2"
+      >
+        <div class="p-4 bg-blue-100 dark:bg-blue-900 rounded">Some other div</div>
+        <div class="p-4 bg-blue-100 dark:bg-blue-900 rounded">Some other div</div>
+        <div
+          ref="child"
+          class="p-4 bg-green-100 dark:bg-green-900 rounded"
+        >
+          Move me to another Parent by clicking the button below
+        </div>
+        <div class="p-4 bg-blue-100 dark:bg-blue-900 rounded">Some other div</div>
+      </div>
+      <div
+        id="parent1"
+        ref="parent1"
+        class="flex flex-col gap-2"
+      >
+        <div class="p-4 bg-blue-100 dark:bg-blue-900 rounded">Some other div</div>
+        <div class="p-4 bg-blue-100 dark:bg-blue-900 rounded">Some other div</div>
+      </div>
     </div>
+    <v-btn @click="toggle">Toggle column</v-btn>
   </div>
-  <div
-    id="parent1"
-    ref="parent1"
-    class="min-h-[10rem]"
-  ></div>
-  <v-btn @click="move(inParent0)">Move to another place</v-btn>
 </template>
 
 <script setup lang="ts">
-import { ref, nextTick } from 'vue'
+import { ref } from 'vue'
 import type { Ref } from 'vue'
-
-const TRANSITION_DURATION = 0.3
 
 const transitionInProgress = ref(false)
 
@@ -33,44 +42,40 @@ const inParent0 = ref(true)
 
 const getRect = (element: HTMLElement) => element.getBoundingClientRect()
 
-const move = async (toParent1: boolean) => {
+const move = async (child: Ref<HTMLElement>, toParent: Ref<HTMLElement>, transitionTime: number = 400) => {
   if (transitionInProgress.value) return
   transitionInProgress.value = true
 
-  if (child.value && parent0.value && parent1.value) {
-    const htmlElementFrom = (toParent1 ? parent0 : parent1) as Ref<HTMLElement>
-    const htmlElementTo = (toParent1 ? parent1 : parent0) as Ref<HTMLElement>
-    const initChild = child as Ref<HTMLElement>
-
-    const initialRect = getRect(htmlElementFrom.value)
+  if (child.value) {
+    const initialRect = getRect(child.value)
 
     const tempChild = ref(document.createElement('div'))
-    htmlElementTo.value.appendChild(tempChild.value)
+    toParent.value.appendChild(tempChild.value)
+
     const finalRect = getRect(tempChild.value)
+
     tempChild.value.remove()
-    console.log(`initialRect: ${JSON.stringify(initialRect)}`)
-    console.log(`finalRect: ${JSON.stringify(finalRect)}`)
 
     const dx = finalRect.left - initialRect.left
     const dy = finalRect.top - initialRect.top
 
     console.log(`dx: ${dx}, dy: ${dy}`)
 
-    const tempStyleTransform = initChild.value.style.transform
-    const tempStyleTransition = initChild.value.style.transition
+    const tempStyleTransform = child.value.style.transform
+    const tempStyleTransition = child.value.style.transition
 
-    initChild.value.style.transition = `transform ${TRANSITION_DURATION}s ease-in-out`
-    initChild.value.style.transform += ' ' + `translate(${dx}px, ${dy}px)`
+    child.value.style.transition = `transform ${transitionTime}ms ease-in-out`
+    child.value.style.transform += ' ' + `translate(${dx}px, ${dy}px)`
 
     // add handler to wait for transition to end
-    initChild.value.addEventListener(
+    child.value.addEventListener(
       'transitionend',
       async (e) => {
         inParent0.value = !inParent0.value
-        htmlElementTo.value.appendChild(initChild.value)
+        toParent.value.appendChild(child.value)
 
-        initChild.value.style.transform = tempStyleTransform
-        initChild.value.style.transition = tempStyleTransition
+        child.value.style.transform = tempStyleTransform
+        child.value.style.transition = tempStyleTransition
 
         transitionInProgress.value = false
       },
@@ -83,4 +88,16 @@ const move = async (toParent1: boolean) => {
   }
 }
 
+function toggle() {
+  if (!parent1.value || !parent0.value || !child.value) {
+    console.error('One of the objects is null')
+    return
+  }
+
+  if (inParent0.value) {
+    move(child as Ref<HTMLElement>, parent1 as Ref<HTMLElement>)
+  } else {
+    move(child as Ref<HTMLElement>, parent0 as Ref<HTMLElement>)
+  }
+}
 </script>
