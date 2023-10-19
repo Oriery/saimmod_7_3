a4cac42eaf0685b274c95332455099dd80f6d8a2
Oriery <panev291002@gmail.com>
2023-10-19 22:15:37 +0300

    more complicated system

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,14 +34,19 @@ console.log(
 
 sysMasSer = new SystemOfMassService()
 
-const generator = new Generator(sysMasSer, 0.3, WhatToDoOnBlockedOutput.WAIT)
-const processor = new Processor(sysMasSer, 0.5, WhatToDoOnBlockedOutput.DROP)
+const generator = new Generator(sysMasSer, 0.2, WhatToDoOnBlockedOutput.WAIT)
+const processor = new Processor(sysMasSer, 0.6, WhatToDoOnBlockedOutput.DROP)
+const processor4 = new Processor(sysMasSer, 0.6, WhatToDoOnBlockedOutput.DROP)
 const queue = new Queue(sysMasSer, 3)
-const processor2 = new Processor(sysMasSer, 0.7)
+const processor2 = new Processor(sysMasSer, 0.8)
+const processor3 = new Processor(sysMasSer, 0.8)
 
 generator.addOutwardNode(processor)
+generator.addOutwardNode(processor4)
 processor.addOutwardNode(queue)
+processor4.addOutwardNode(queue)
 queue.addOutwardNode(processor2)
+queue.addOutwardNode(processor3)
 
 interval = setInterval(() => {
   if (sysMasSer) {
